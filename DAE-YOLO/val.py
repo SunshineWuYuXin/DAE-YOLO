3a82cb216649d43da5c7847817878237e2026ad5
SunshineWuYuXin <139879492+SunshineWuYuXin@users.noreply.github.com>
2025-05-23 13:35:08 +0800

    Delete DAE-YOLO directory

--- DAE-YOLO/val.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-from ultralytics import YOLO
-import matplotlib
-
-
-if __name__ == '__main__':
-
-    model = YOLO('/root/autodl-tmp/runs/train/exp/weights/best.pt')
-    metrics = model.val(data = '/root/autodl-tmp/ultralytics/cfg/datasets/DOTA.yaml')
